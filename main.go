75fe483250d2a60ef640a83cab72128ab87fd967
BlueSkyCaps <linsongxian@outlook.com>
2023-07-29 21:06:13 +0800

    改为‘文泉驿微米黑’字体。

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 )
 
 func init() {
+
 	storagejson.AppRef.A = app.NewWithID("top.reminisce.pasecret")
 	t := &config.DefaultGlobalSettingTheme{}
-	t.SetFonts("STXINWEI.TTF", resourceSTXINWEITTF.StaticContent)
+	t.SetFonts("WenQuanWeiMiHei.ttf", resourceWenQuanWeiMiHeiTtf.StaticContent)
 	// 更新主题，让fyne使用自定义主题配置
 	storagejson.AppRef.A.Settings().SetTheme(t)
 	storagejson.AppRef.W = storagejson.AppRef.A.NewWindow("Pasecret")
@@ -20,6 +21,7 @@ func init() {
 		//窗体宽度会由子容器grid自动适应
 		storagejson.AppRef.W.Resize(fyne.Size{Height: 540})
 	}
+
 }
 
 func main() {
