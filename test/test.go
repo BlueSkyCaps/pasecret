d13712a13bde44fd6e3a33378c182e78b5e69eb4
BlueSkyCaps <linsongxian@outlook.com>
2023-08-29 22:55:26 +0800

    11

--- test/test.go
+++ test/test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fyne.io/fyne/v2"
-	"pasecret/core/common"
+	"time"
 )
 
 func SendNotification(s string) {
@@ -13,10 +13,30 @@ func SendNotification(s string) {
 }
 
 func main() {
-	println(common.EncryptAES([]byte(common.AppProductKeyAES), "1234"))
-	println(common.EncryptAES([]byte(common.AppProductKeyAES), "1234"))
-	println(common.DecryptAES([]byte(common.AppProductKeyAES), "vHg+/9NnScW6M1nYdm/MyU5loM4="))
-	println(common.DecryptAES([]byte(common.AppProductKeyAES), "IZGyzjZf+/RcLnx6pF/vW5ezlEA=1"))
+	c := make(chan int, 2)
+	go func() {
+		time.Sleep(2 * time.Second)
+		c <- 1
+		defer println("111")
+	}()
+	go func() {
+		time.Sleep(4 * time.Second)
+		c <- 1
+		close(c)
+		defer println("111")
+	}()
+	<-c
+	time.Sleep(6 * time.Second)
+	println("222")
+
+	<-c
+	println("222")
+
+	//
+	//println(common.EncryptAES([]byte(common.AppProductKeyAES), "1234"))
+	//println(common.EncryptAES([]byte(common.AppProductKeyAES), "1234"))
+	//println(common.DecryptAES([]byte(common.AppProductKeyAES), "vHg+/9NnScW6M1nYdm/MyU5loM4="))
+	//println(common.DecryptAES([]byte(common.AppProductKeyAES), "IZGyzjZf+/RcLnx6pF/vW5ezlEA=1"))
 
 	//c := "12345"
 	//println(strconv.Atoi(string(c[0])))
